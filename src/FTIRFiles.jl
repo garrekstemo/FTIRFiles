633018d795cd280da810037456dbd9bb0e183e1a
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2023-01-12 19:21:12 +0900

    Spectrum type, ready to use

--- src/FTIRFiles.jl
+++ src/FTIRFiles.jl
@@ -8,40 +8,6 @@ using StringEncodings
 export read_spectrum,
        Spectrum
 
-"""
-    read_spectrum(file, names = ["X", "Y"])
-"""
-function read_spectrum(file, names = ["X", "Y"])
-    #TODO: Get meta-data from the spectrum file.
-    datarows = 0
-
-    metadata = CSV.File(file, limit=18)
-    for row in metadata
-        if row.TITLE == "XUNITS"
-            if row.Column2 == "1/CM"
-                names[1] = "Wavenumber"
-            end
-        end
-
-        if row.TITLE == "YUNITS"
-            names[2] = titlecase(row.Column2)
-        end
-
-        if row.TITLE == "NPOINTS"
-            datarows = parse(Int, row.Column2)
-        end
-    end
-
-    if datarows > 0
-        df = DataFrame(CSV.File(file, skipto=20, limit=datarows))
-    else
-        df = DataFrame(CSV.File(file, skipto=20, footerskip=17)) 
-    end
-
-    rename!(df, names)
-    return df
-end
-
 struct Spectrum
     title::String
     datatype::String
@@ -49,43 +15,75 @@ struct Spectrum
     owner::String
     date::Date
     time::Time
+    spectrometer::String
     locale::Int64
+    resolution::String
     deltax::Float64
     xunits::String
     yunits::String
+    firstx::Float64
+    lastx::Float64
+    npoints::Int64
+    firsty::Float64
+    maxy::Float64
+    miny::Float64
+    x::Vector{Float64}
+    y::Vector{Float64}
+end
 
-    # the inner constructor should not be used directly
-    function Spectrum(path)
-        f = open(path, enc"SHIFT-JIS", "r")
-        lines = readlines(f)
-        for line in lines
-            l = split(line, ",")
-            if length(l)
-                if l[1] == "TITLE"
-                    title = l[2]
-                elseif l[1] == "DATATYPE"
-                    datatype = l[2]
-                elseif l[1] == "ORIGIN"
-                    origin = l[2]
-                elseif l[1] == "OWNER"
-                    owner = l[2]
-                elseif l[1] == "DATE"
-                    date = Date(l[2], "yy/mm/dd") + Dates.Year(2000)
-                elseif l[1] == "TIME"
-                    time = Time(l[2], "HH:MM:SS")
-                elseif l[1] == "LOCALE"
-                    locale = parse(Int, l[2])
-                elseif l[1] == "DELTAX"
-                    deltax = parse(Float64, l[2])
-                elseif l[1] == "XUNITS"
-                    xunits = l[2]
-                elseif l[1] == "YUNITS"
-                    yunits = l[2]
+function Spectrum(path::String)
+    f = open(path, enc"SHIFT-JIS", "r")
+    lines = readlines(f)
+    metadata = Dict()
+    for line in lines
+        splitline = split(line, ",")
+        if length(splitline) == 2
+            # We only want metadata, but the spectrum is also a comma-separated xy pair
+            try
+                parse(Float64, splitline[1])
+            catch
+                if splitline[1] == "DATE"
+                    metadata[splitline[1]] = Date(splitline[2], "yy/mm/dd") + Year(2000)
+                elseif splitline[1] == "TIME"
+                    metadata[splitline[1]] = Time(splitline[2], "HH:MM:SS")
+                else
+                    metadata[splitline[1]] = splitline[2]
                 end
             end
         end
-        close(f)
     end
+
+    npoints = parse(Int64, metadata["NPOINTS"])
+    xdata = zeros(npoints)
+    ydata = zeros(npoints)
+    start = findfirst(line -> line == "XYDATA", lines)
+
+    for (i, line) in enumerate(lines[start + 1:start + npoints])
+        xdata[i] = parse(Float64, split(line, ",")[1])
+        ydata[i] = parse(Float64, split(line, ",")[2])
+    end
+
+    return Spectrum(metadata["TITLE"],
+                    metadata["DATA TYPE"],
+                    metadata["ORIGIN"],
+                    metadata["OWNER"],
+                    metadata["DATE"],
+                    metadata["TIME"],
+                    metadata["SPECTROMETER/DATA SYSTEM"],
+                    parse(Int64, metadata["LOCALE"]),
+                    metadata["RESOLUTION"],
+                    parse(Float64, metadata["DELTAX"]),
+                    metadata["XUNITS"],
+                    metadata["YUNITS"],
+                    parse(Float64, metadata["FIRSTX"]),
+                    parse(Float64, metadata["LASTX"]),
+                    parse(Int64, metadata["NPOINTS"]),
+                    parse(Float64, metadata["FIRSTY"]),
+                    parse(Float64, metadata["MAXY"]),
+                    parse(Float64, metadata["MINY"]),
+                    xdata,
+                    ydata
+    )
 end
 
 
