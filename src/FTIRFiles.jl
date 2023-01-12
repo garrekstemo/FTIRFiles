bccc1c8a43337e1d5bcf1c5fc1fdfd3a4ca3f661
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2023-01-12 17:49:59 +0900

    Add Spectrum type

--- src/FTIRFiles.jl
+++ src/FTIRFiles.jl
@@ -2,8 +2,11 @@ module FTIRFiles
 
 using CSV
 using DataFrames
+using Dates
+using StringEncodings
 
-export read_spectrum
+export read_spectrum,
+       Spectrum
 
 """
     read_spectrum(file, names = ["X", "Y"])
@@ -12,7 +15,7 @@ function read_spectrum(file, names = ["X", "Y"])
     #TODO: Get meta-data from the spectrum file.
     datarows = 0
 
-    metadata = CSV.File(datafile, limit=18)
+    metadata = CSV.File(file, limit=18)
     for row in metadata
         if row.TITLE == "XUNITS"
             if row.Column2 == "1/CM"
@@ -30,13 +33,59 @@ function read_spectrum(file, names = ["X", "Y"])
     end
 
     if datarows > 0
-        df = DataFrame(CSV.File(datafile, skipto=20, limit=datarows))
+        df = DataFrame(CSV.File(file, skipto=20, limit=datarows))
     else
-        df = DataFrame(CSV.File(datafile, skipto=20, footerskip=17)) 
+        df = DataFrame(CSV.File(file, skipto=20, footerskip=17)) 
     end
 
     rename!(df, names)
     return df
+end
+
+struct Spectrum
+    title::String
+    datatype::String
+    origin::String
+    owner::String
+    date::Date
+    time::Time
+    locale::Int64
+    deltax::Float64
+    xunits::String
+    yunits::String
+
+    # the inner constructor should not be used directly
+    function Spectrum(path)
+        f = open(path, enc"SHIFT-JIS", "r")
+        lines = readlines(f)
+        for line in lines
+            l = split(line, ",")
+            if length(l)
+                if l[1] == "TITLE"
+                    title = l[2]
+                elseif l[1] == "DATATYPE"
+                    datatype = l[2]
+                elseif l[1] == "ORIGIN"
+                    origin = l[2]
+                elseif l[1] == "OWNER"
+                    owner = l[2]
+                elseif l[1] == "DATE"
+                    date = Date(l[2], "yy/mm/dd") + Dates.Year(2000)
+                elseif l[1] == "TIME"
+                    time = Time(l[2], "HH:MM:SS")
+                elseif l[1] == "LOCALE"
+                    locale = parse(Int, l[2])
+                elseif l[1] == "DELTAX"
+                    deltax = parse(Float64, l[2])
+                elseif l[1] == "XUNITS"
+                    xunits = l[2]
+                elseif l[1] == "YUNITS"
+                    yunits = l[2]
+                end
+            end
+        end
+        close(f)
+    end
 end
 
 
